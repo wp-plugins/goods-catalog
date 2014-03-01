6e819d61c1d165e787163e163011e8fc8acbc553
oriolo <oriolo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-01 05:02:14 +0000

    renamed tags/0.4
    git-svn-id: https://plugins.svn.wordpress.org/goods-catalog/tags/0.4@867368 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== Goods Catalog ===
+Contributors: oriolo
+Tags: catalog, products, goods, products catalog, goods catalog
+Stable tag: v0.4
+Requires at least: 3.3.0
+Tested up to: 3.8.1
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The plugin creates simple catalog of goods, looking like Explorer in Windows.
+
+== Description ==
+The plugin creates simple catalog of goods, looking like Explorer in Windows. The main page of catalog is http://yoursite.com/catalog/. At the main page are located all parent categories of goods. At the category page firstly are located subcategories and then products of the category.
+
+* Use thumbnails to add images for products;
+* You can use Taxonomy Images plugin (https://wordpress.org/plugins/taxonomy-images/) to add images for categories of products;
+
+Languages:
+
+* English
+* Russian
+
+== Installation ==
+Download plugin in .zip archive and install in Console. Then you need to update your permalinks to prevent 404 errors. Just go to Console -> Settings -> Permalinks and press "Save". You don't need to change it.
+
+== Screenshots ==
+1. http://oriolo.ru/wp-content/uploads/2014/02/1-540x290.jpg
+2. http://oriolo.ru/wp-content/uploads/2014/02/3.jpg
+3. http://oriolo.ru/wp-content/uploads/2014/02/4.jpg
+
+== Frequently Asked Questions ==
+= There is 404 on /catalog or products page =
+You need to update your permalinks. Just go to Console -> Settings -> Permalinks and press "Save". You don't need to change it.
+
+== Changelog ==
+
+= v0.4 =
+
+* Bugs fixed
+* Updated readme.txt
+
+= v0.3-beta.2 =
+
+Small bug fix:
+
+* Fixed missed menu in category page
+
+= v0.3-beta.1 =
+
+* Fixed pagination
+
+= v0.3-beta =
+
+* Added Plugin Settings page
+* Change products per page
+* Show categories images if plugin Taxonomy Images not installed
+
+= v0.2-beta.1 =
+
+Small update:
+* removed displaying of PHP errors
+
+= v0.2-beta =
+
+First public release
---
+++ taxonomy-goods_category.php
@@ -0,0 +1,104 @@
+<?php
+get_header();
+$term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+
+if (have_posts()) {
+    ?>
+    <div class="breadcrumbs">
+        <?php
+        if (!is_search() || !is_404()) {
+            global $post;
+            if ($post != null) {
+                my_breadcrumb($post->post_parent);
+            } else {
+                my_breadcrumb();
+            }
+        } else {
+            print ' ';
+        }
+        ?>
+    </div>
+    <?php
+    global $posts;
+    $post = $posts[0];
+
+    ob_start();
+
+    echo '<div class="single-category-title">' . single_cat_title('', false) . '</div>';
+    echo '<p>' . category_description() . '</p>';
+
+    // show sub-categories only in first page, if paged
+    if (!is_paged()) {
+        // show sub-categories list
+        $current_term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+        $args = array(
+            'parent' => $current_term->term_id,
+            'taxonomy' => $current_term->taxonomy,
+            'hide_empty' => 0,
+            'hierarchical' => true,
+            'depth' => 1
+        );
+
+        $category_list = get_categories($args);
+        // include
+        include 'content-goods_category.php';
+
+        echo "<hr>";
+    }
+
+// Start the Loop
+while ( have_posts() ) {
+		the_post();
+        ?>
+        <div class="grid">
+            <article <?php post_class(); ?>>
+                <div class="goods-item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+                <div class="goods-item-content">
+                    <?php
+                    // show thumbnails
+                    echo '<div class="goods-item-thumb-container">';
+                    if (has_post_thumbnail()) {
+                        echo '<a href="' . get_permalink() . '">';
+                        the_post_thumbnail(array(150, 150), array('class' => 'goods-item-thumb'));
+                        echo '</a>';
+                    } else {
+                        // show default image if the thumbnail is not found
+                        echo '<a href="' . get_permalink() . '"><img class="goods-item-thumb" src="' . plugins_url('img/gi.png', __FILE__) . '" alt=""></a>';
+                    }
+                    echo '</div>';
+                    // get price and description from metabox
+                    $price = get_post_meta(get_the_ID(), 'gc_price', true);
+                    $descr = get_post_meta(get_the_ID(), 'gc_descr', true);
+                    // show price and description
+                    if ((isset($price)) && ($price != '')) {
+                        echo "<div class=\"goods-price-single\">";
+                        echo __('Price:', 'gcat');
+                        echo " $price</div>";
+                    }
+                    if ((isset($descr)) && ($descr != '')) {
+                        echo "<div class=\"goods-descr\">$descr</div>";
+                    }
+                    ?>
+                </div>
+            </article>
+        </div>
+
+        <?php
+    }
+}
+ else {
+    echo __('There are no products in the category.', 'gcat');
+}
+// Display navigation to next/previous pages when applicable
+?>
+
+<div class="clear"></div>
+<div class="navigation">
+    <?php if (function_exists('goods_pagination'))
+        goods_pagination();
+    else
+        posts_nav_link();
+    ?>
+</div>
+<?php
+get_footer();
