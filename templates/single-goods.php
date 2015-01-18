1d56aab2af45a2808fd15d8a030d4b1073b2d127
oriolo <oriolo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-18 10:28:45 +0000

    0.6.8
    git-svn-id: https://plugins.svn.wordpress.org/goods-catalog/tags/0.6.8@1070319 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/single-goods.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * Template: Single product page
+ */
+
+get_header();
+echo '<div class="goods-catalog-container">';
+load_template(dirname(__FILE__) . '/sidebar-goods.php');
+
+echo '<div class="goods-catalog">';
+echo '<div class="catalog-inner">';
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        // Display navigation next/previous
+        ?>
+        <div class="breadcrumbs">
+            <?php
+            if (!is_search() || !is_404()) {
+                global $post;
+                if ($post != null) {
+                    my_breadcrumb($post->post_parent);
+                } else {
+                    my_breadcrumb();
+                }
+            } else {
+                print ' ';
+            }
+            ?>
+        </div>
+
+        <article <?php post_class(); ?>>
+            <header>
+                <?php
+                echo '<div class="goods-single-thumb-container">';
+                if (has_post_thumbnail()) {
+                    $large_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large');
+                    echo '<a href="' . $large_image_url[0] . '" title="' . the_title_attribute('echo=0') . '" >';
+                    the_post_thumbnail('medium', array('class' => 'goods-single-thumb'));
+                    echo '</a>';
+                } else {
+                    // show default image if the thumbnail is not found
+                    echo '<img class="goods-item-thumb" src="' . plugins_url( '/img/gi.png' , dirname(__FILE__) ) . '" alt="">';
+                }
+                echo '</div>';
+                ?>
+                <div class="goods-info">
+                    <h2 class="entry-title"><?php the_title(); ?></h2>
+                    <?php
+                    // show product
+                    show_the_product_price();
+                    
+                    if (isset($catalog_option['show_product_sku_page'])) {
+                        show_the_product_sku();
+                    }
+                    if (isset($catalog_option['show_product_descr_page'])) {
+                        show_the_product_desrc();
+                    }
+
+                    // show category
+                    echo '<p>';
+                    get_goods_taxomonies('goods_category', $post->ID);
+                    echo '</p>';
+
+                    // show tags
+                    echo '<p>';
+                    get_goods_taxomonies('goods_tag', $post->ID);
+                    echo '</p>';
+                    ?>
+                </div>
+                <div class="clear"></div>
+            </header>
+            <div class="entry-content">
+                <?php
+                the_content();
+                ?>
+            </div>
+            <div class="clear"></div>
+        </article>
+        <?php
+    }
+
+    // Display navigation to next/previous
+    ?>
+    <div class="navigation"><?php posts_nav_link(); ?></div>
+    <?php
+    echo '</div>';
+} else {
+    get_404_template();
+}
+echo '</div>';
+echo '<div class="clear"></div>'; // fix for some themes
+echo '</div>'; // goods-catalog-container
+get_footer();
